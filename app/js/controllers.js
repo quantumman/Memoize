64d0293895c59b85b884ebc7703d41a13451eb25
Shodai Yokoyama <quantumcars@gmail.com>
2014-08-22 08:38:44 +0900

    Show a page to list notes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,4 +12,24 @@ memoizeControllers.controller('NavbarController', ['$scope', '$location',
 
 memoizeControllers.controller('ListController', ['$scope',
   function($scope) {
+    $scope.notes = [
+      {
+        thumbnail: '#',
+        editor: 'TEST',
+        title: 'TEST TITLE #1',
+        createdDate: '2014-08-26T10:10:10'
+      },
+      {
+        thumbnail: '#',
+        editor: 'fugahoge',
+        title: 'TEST TITLE #2',
+        createdDate: '2014-08-24T20:20:20'
+      },
+      {
+        thumbnail: '#',
+        editor: 'hoge',
+        title: 'TEST TITLE #3',
+        createdDate: '2014-09-24T20:20:20'
+      }
+    ];
   }]);
