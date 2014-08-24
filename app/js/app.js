a83321b8161a6bb6f281aa307a0884d2096b12dd
Shodai Yokoyama <quantumcars@gmail.com>
2014-08-25 01:29:10 +0900

    Define routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,18 @@
 var memoizeApp = angular.module('memoizeApp', [
+  'ngRoute',
   'ui.bootstrap'
 ]);
+
+memoizeApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/list', {
+        templateUrl: 'partials/list.html'
+      })
+      .when('/notes/edit', {
+        templateUrl: 'partials/notes-edit.html'
+      })
+      .otherwise({
+        redirectTo: '/list'
+      });
+  }]);
