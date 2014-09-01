0bdcff5e4783e2c2276cf558e088de3f80145f70
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-01 23:50:12 +0900

    note.html: Use images

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,19 +14,19 @@ memoizeControllers.controller('ListController', ['$scope',
   function($scope) {
     $scope.notes = [
       {
-        thumbnail: '#',
+        thumbnail: 'img/1.jpg',
         editor: 'TEST',
         title: 'TEST TITLE #1',
         createdDate: '2014-08-26T10:10:10'
       },
       {
-        thumbnail: '#',
+        thumbnail: 'img/2.jpg',
         editor: 'fugahoge',
         title: 'TEST TITLE #2',
         createdDate: '2014-08-24T20:20:20'
       },
       {
-        thumbnail: '#',
+        thumbnail: 'img/1.jpg',
         editor: 'hoge',
         title: 'TEST TITLE #3',
         createdDate: '2014-09-24T20:20:20'
