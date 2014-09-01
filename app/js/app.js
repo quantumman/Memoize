0f2df1d9bedc35ab30a9b440726d8c1724914721
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-01 21:51:24 +0900

    Define routing for notes/request

--- app/js/app.js
+++ app/js/app.js
@@ -11,6 +11,10 @@ memoizeApp.config(['$routeProvider',
         templateUrl: 'partials/list.html',
         controller: 'ListController'
       })
+      .when('/notes/request', {
+        templateUrl: 'partials/notes-request.html',
+        controller: 'RequestController'
+      })
       .when('/notes/edit', {
         templateUrl: 'partials/notes-edit.html'
       })
