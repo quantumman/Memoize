9934a47a4a54f3c49c125feb7b7bb224472558ce
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-01 23:50:51 +0900

    Show a list of requests for notes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -36,4 +36,44 @@ memoizeControllers.controller('ListController', ['$scope',
 
 memoizeControllers.controller('RequestController', ['$scope',
   function($scope) {
+    $scope.notes = [
+      {
+        thumbnail: 'img/1.jpg',
+        title: 'TEST TITLE #1',
+        createdDate: '2014-08-26T10:10:10',
+        requests: [
+          {
+            thumbnail: 'img/2.jpg',
+            contributor: 'fugahoge',
+            comment: 'Fix it!',
+            createdDate: '2014-09-29T10:10'
+          },
+          {
+            thumbnail: 'img/1.jpg',
+            contributor: 'Hoge',
+            comment: 'Fix it!',
+            createdDate: '2014-10-29T10:10'
+          },
+          {
+            thumbnail: 'img/2.jpg',
+            contributor: 'Fuga',
+            comment: 'Fix it!',
+            createdDate: '2014-11-29T10:10'
+          }
+        ]
+      },
+      {
+        thumbnail: 'img/2.jpg',
+        title: 'TEST TITLE #2',
+        createdDate: '2014-02-26T10:10:10',
+        requests: [
+          {
+            thumbnail: 'img/1.jpg',
+            contributor: 'Bar',
+            comment: 'Add funny content!',
+            createdDate: '2014-04-29T10:10'
+          }
+        ]
+      }
+    ];
   }]);
