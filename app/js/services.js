e4e83ab89b5e60573f77ed3c86eafe63e9343be8
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-14 22:24:16 +0900

    services.js: Add Notes

--- app/js/services.js
+++ app/js/services.js
@@ -2,3 +2,11 @@
 
 /* Services */
 var memoizeServices = angular.module('memoizeServices', ['ngResource']);
+
+memoizeServices.factory('Notes', ['$resource',
+  function($resource) {
+    return $resource('notes/:noteId.json', {}, {
+      list: {method: 'GET', params:{noteId: 'notes'}, isArray: true}
+    });
+  }
+]);
