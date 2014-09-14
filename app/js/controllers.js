8ac06de677afd8ba101270492704e64a21eb464a
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-14 22:25:57 +0900

    controller.js: ListController - Use Notes to fetch data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,28 +10,9 @@ memoizeControllers.controller('NavbarController', ['$scope', '$location',
     };
   }]);
 
-memoizeControllers.controller('ListController', ['$scope',
-  function($scope) {
-    $scope.notes = [
-      {
-        thumbnail: 'img/1.jpg',
-        editor: 'TEST',
-        title: 'TEST TITLE #1',
-        createdDate: '2014-08-26T10:10:10'
-      },
-      {
-        thumbnail: 'img/2.jpg',
-        editor: 'fugahoge',
-        title: 'TEST TITLE #2',
-        createdDate: '2014-08-24T20:20:20'
-      },
-      {
-        thumbnail: 'img/1.jpg',
-        editor: 'hoge',
-        title: 'TEST TITLE #3',
-        createdDate: '2014-09-24T20:20:20'
-      }
-    ];
+memoizeControllers.controller('ListController', ['$scope', 'Notes',
+  function($scope, Notes) {
+    $scope.notes = Notes.list();
   }]);
 
 memoizeControllers.controller('RequestController', ['$scope',
