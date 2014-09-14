57e4648420bb39275127485f8eb2f4adc48e9a11
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-15 02:32:09 +0900

    services.js: Define default parameter to use $resouces.get

--- app/js/services.js
+++ app/js/services.js
@@ -5,7 +5,7 @@ var memoizeServices = angular.module('memoizeServices', ['ngResource']);
 
 memoizeServices.factory('Notes', ['$resource',
   function($resource) {
-    return $resource('notes/:noteId.json', {}, {
+    return $resource('notes/:noteId.json', {noteId: '@id'}, {
       list: {method: 'GET', params:{noteId: 'notes'}, isArray: true},
       requests: {method: 'GET', params:{noteId: 'requests'}, isArray: true}
     });
