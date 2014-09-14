2363a7cdbdb319b96f8b13d3ebc253a26681e2bc
Shodai Yokoyama <quantumcars@gmail.com>
2014-09-14 22:47:44 +0900

    services.js: Add reqeusts

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,8 @@ var memoizeServices = angular.module('memoizeServices', ['ngResource']);
 memoizeServices.factory('Notes', ['$resource',
   function($resource) {
     return $resource('notes/:noteId.json', {}, {
-      list: {method: 'GET', params:{noteId: 'notes'}, isArray: true}
+      list: {method: 'GET', params:{noteId: 'notes'}, isArray: true},
+      requests: {method: 'GET', params:{noteId: 'requests'}, isArray: true}
     });
   }
 ]);
